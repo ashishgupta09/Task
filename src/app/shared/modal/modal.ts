ff673ffb8e35eabbbb5270527cc4f67cda7cc957
ashish09 <ashishgupta95652@gmail.com>
2026-08-20 23:58:15 +0530

    added the modal...

--- src/app/shared/modal/modal.ts
+++ src/app/shared/modal/modal.ts
@@ -1,9 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, ContentChild, HostListener, input, output, TemplateRef } from '@angular/core';
+import { NgTemplateOutlet } from "../../../../node_modules/@angular/common/types/_common_module-chunk";
 
 @Component({
-  imports: [],
+  imports: [NgTemplateOutlet],
   selector: 'app-modal',
   styleUrl: './modal.css',
   templateUrl: './modal.html',
 })
-export class Modal {}
+export class Modal {
+
+  readonly isOpen = input<boolean>(false);
+  readonly title = input<string>('');
+  readonly size = input<'sm' | 'md' | 'lg'>('md')
+
+  readonly closed = output<void>();
+
+  @ContentChild('modalBody') bodyTpl!: TemplateRef<unknown>;
+  @ContentChild('modalFooter') footerTpl!: TemplateRef<unknown>;
+
+  close() {
+    this.closed.emit();
+  }
+
+  onBackdropClick(event: MouseEvent): void {
+    if ((event.target as HTMLElement).classList.contains('modal-backdrop')) {
+      this.close();
+    }
+  }
+
+  @HostListener('document:keydown.escape')
+  onEscape(): void {
+    if (this.isOpen()) {
+      this.close();
+    }
+  }
+
+}
