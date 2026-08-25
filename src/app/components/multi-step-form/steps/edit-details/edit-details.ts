f41700f59f78175d92f4442339d9bb90745698c5
ashish09 <ashishgupta95652@gmail.com>
2026-08-25 20:13:58 +0530

    feat: add two more steps to multi-step form.

--- src/app/components/multi-step-form/steps/edit-details/edit-details.ts
+++ src/app/components/multi-step-form/steps/edit-details/edit-details.ts
@@ -1,9 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, effect, inject, input, output } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { PersonalFormDetails } from '../../interfaces/forms-details';
 
 @Component({
-  imports: [],
+  imports: [ReactiveFormsModule],
   selector: 'app-edit-details',
   styleUrl: './edit-details.css',
   templateUrl: './edit-details.html',
+  standalone: true,
 })
-export class EditDetails {}
+export class EditDetails {
+
+  readonly details = input.required<PersonalFormDetails>();
+  readonly next = output<PersonalFormDetails>();
+  readonly back = output<PersonalFormDetails>();
+  private readonly fb = inject(FormBuilder);
+
+  form: FormGroup = this.fb.group({
+    firstName: ['', [Validators.required, Validators.minLength(2)]],
+    lastName: ['', [Validators.required, Validators.minLength(2)]],
+    email: ['', [Validators.required, Validators.email]],
+    phone: ['', [Validators.required, Validators.pattern(/^\+?[\d\s\-()]{7,15}$/)]],
+  })
+
+  constructor() {
+    effect(() => {
+      const formDetails = this.details();
+      if (formDetails) {
+        this.form.patchValue({
+          firstName: formDetails.firstName || '',
+          lastName: formDetails.lastName || '',
+          email: formDetails.email || '',
+          phone: formDetails.phone || '',
+        }, { emitEvent: false }
+        )
+      }
+    })
+  }
+
+  onNext() {
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      return
+    }
+    this.next.emit(this.form.value as PersonalFormDetails)
+  }
+
+  onBack() {
+    this.back.emit(this.form.value as PersonalFormDetails)
+  }
+
+  get firstName() { return this.form.get('firstName')! };
+  get lastName() { return this.form.get('lastName')! }
+  get email() { return this.form.get('email')! }
+  get phone() { return this.form.get('phone')! }
+  
+}
--- src/app/components/multi-step-form/steps/review-details/review-details.ts
+++ src/app/components/multi-step-form/steps/review-details/review-details.ts
@@ -1,9 +1,28 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject, output } from '@angular/core';
+import { FormDraftStore } from '../../data-access/form-draft-store';
 
 @Component({
-  imports: [],
+  imports: [CommonModule],
   selector: 'app-review-details',
   styleUrl: './review-details.css',
   templateUrl: './review-details.html',
+  standalone: true
 })
-export class ReviewDetails {}
+export class ReviewDetails {
+
+  private readonly formDataService = inject(FormDraftStore);
+  readonly formStare = this.formDataService.formSignalState;
+  readonly fullName = this.formDataService.fullName;
+
+  readonly submit = output<void>();
+  readonly back = output<void>();
+
+  onSubmit(): void {
+    this.submit.emit()
+  }
+
+  onBack(): void {
+    this.back.emit()
+  }
+}
